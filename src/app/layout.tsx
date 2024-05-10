84a848a3ce10f9891663bf1fe2222bdbcf07c13b
Dan Warrick <danwarrickdev@gmail.com>
2024-05-10 18:51:12 -0400

    favicon and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ export const metadata = {
   title: "Detroit Event Map",
   description:
     "Find local events, music, concerts, art shows and things to do in the metro Detroit area",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({

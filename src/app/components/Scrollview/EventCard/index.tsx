12ba396b6ed06373d0578862fd23ed52a1ffb667
Dan Warrick <danwarrickdev@gmail.com>
2024-05-15 19:09:39 -0400

    updated event card styling

--- src/app/components/Scrollview/EventCard/index.tsx
+++ src/app/components/Scrollview/EventCard/index.tsx
@@ -1,94 +1,78 @@
-import { Box, Image, Stack, HStack, useTheme } from '@chakra-ui/react';
-import { H4, Paragraph } from '../../Typography';
-import { Info } from '@phosphor-icons/react';
+import { HStack, Text, Flex, Heading, IconButton, Box } from '@chakra-ui/react';
+import { ArrowSquareOut } from '@phosphor-icons/react';
 import { EventData } from '@/app/types';
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
+import Link from 'next/link';
 
 export const EventCard = ({ event }: { event: EventData }) => {
   const { title, img, location, price, url } = event;
 
-  const theme = useTheme();
-
   return (
-    <Box height="100%" width="100%" py={'.25rem'}>
-      <Image
-        src={img}
-        height="100%"
-        width="100%"
-        objectFit={'cover'}
-        borderRadius={'1rem'}
-        shadow={'1px 1px 5px black'}
-      />
-      <Stack zIndex={99} position={'absolute'} top={0} left={0} right={0}>
-        <Box
-          height={'5rem'}
-          display={'flex'}
-          justifyContent={'center'}
-          alignItems={'center'}
-          backgroundColor={'black'}
-          shadow={'0px 4px 16px rgba(255, 255, 255, 0.7)'}
-          overflow={'hidden'}
-          textOverflow={'ellipsis'}
-          p=".5rem"
-          borderTopRadius={'1rem'}
-        >
-          <H4
-            color={'white'}
-            textAlign={'center'}
-            fontFamily={theme.fonts.body}
-            noOfLines={2}
-            overflow={'hidden'}
-            maxH={'4rem'}
-          >
-            {title}
-          </H4>
-        </Box>
-        <Box
-          backgroundColor={'black'}
-          maxW={'48px'}
-          p={'.5rem'}
-          borderRadius={'.25rem'}
-          shadow={'0px 4px 16px rgba(255, 255, 255, 0.7)'}
-          m={'.5rem'}
-        >
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            <Info color={'white'} size={32} />
-          </a>
-        </Box>
-      </Stack>
-      <HStack position={'absolute'} bottom={'.5rem'} zIndex={99} left={'.5rem'}>
-        <Paragraph
-          opacity={1}
-          color={'white'}
-          backgroundColor={'black'}
-          borderRadius={'.25rem'}
-          padding={'.5rem'}
-          display={'block'}
-          shadow={'0px 4px 16px rgba(255, 255, 255, 0.7)'}
-          maxW={'22dvh'}
-          noOfLines={2}
-          overflow={'hidden'}
-          maxH={'4rem'}
-        >
-          {location}
-        </Paragraph>
-        {price && (
-          <Paragraph
-            opacity={1}
-            color={'white'}
-            backgroundColor={'black'}
-            borderRadius={'.25rem'}
+    <Card size={'md'} backgroundColor={'#FEFEFE'}>
+      <CardHeader
+        h={'4rem'}
+        paddingTop={0}
+        backgroundColor={'#9D121A'}
+        color="#FEFEFE"
+        borderTopRadius={'inherit'}
+      >
+        <Flex alignItems="center" height={'4rem'} w="100%">
+          <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+            <Heading size="md" textOverflow={'ellipsis'} noOfLines={2}>
+              {title}
+            </Heading>
+          </Flex>
+        </Flex>
+      </CardHeader>
+
+      <CardBody
+        backgroundImage={img}
+        backgroundPosition={'top'}
+        minH={{ base: '10rem', md: '15rem' }}
+      ></CardBody>
+      <CardFooter h={'5rem'}>
+        <HStack width="100%">
+          <Text
+            color="white"
+            backgroundColor={'#9D121A'}
             padding={'.5rem'}
-            display={'block'}
-            shadow={'0px 4px 16px rgba(255, 255, 255, 0.7)'}
-            maxW={'22dvh'}
-            noOfLines={2}
+            borderRadius={'.25rem'}
+            textOverflow={'ellipsis'}
+            noOfLines={1}
             overflow={'hidden'}
-            maxH={'4rem'}
+            fontSize={{ base: '.8rem', md: '1rem' }}
+            textAlign={'center'}
           >
-            {price}
-          </Paragraph>
-        )}
-      </HStack>
-    </Box>
+            {location}
+          </Text>
+          {price && (
+            <Text
+              color="white"
+              backgroundColor={'#9D121A'}
+              padding={'.5rem'}
+              borderRadius={'.25rem'}
+              textOverflow={'ellipsis'}
+              noOfLines={{ base: 1, md: 1 }}
+              maxW={'50%'}
+              overflow={'hidden'}
+              fontSize={{ base: '.8rem', md: '1rem' }}
+              textAlign={'center'}
+            >
+              {price}
+            </Text>
+          )}
+        </HStack>
+        <Box backgroundColor={'#282929'} borderRadius={'5rem'}>
+          <Link href={url} target="_blank">
+            <IconButton
+              variant="ghost"
+              colorScheme="gray"
+              aria-label="See menu"
+              icon={<ArrowSquareOut size={28} color={'#FEFEFE'} />}
+            />
+          </Link>
+        </Box>
+      </CardFooter>
+    </Card>
   );
 };

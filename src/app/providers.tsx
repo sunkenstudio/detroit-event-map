7c0c19b35f63493897aadbc4b547bedc9775a45d
Daniel Warrick <danielwarrick@Daniels-MacBook-Pro.local>
2024-07-31 18:33:47 -0400

    unneeded dependency

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,8 @@
 'use client';
 import { ChakraProvider } from '@chakra-ui/react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import theme from './styles/theme';
 import React from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_API_ID as string;
-  return (
-    <GoogleOAuthProvider clientId={googleClientId}>
-      <ChakraProvider theme={theme}>{children}</ChakraProvider>
-    </GoogleOAuthProvider>
-  );
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
